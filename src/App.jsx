fe8a791437f0f330ae10b9ab084abfba890c17df
Bhagyashree <bshree2604@gmail.com>
2024-10-24 14:05:43 +0530

    cleanup gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 function App() {
   return (
     <>
+      <h1 className='text-3xl text-gray-100 text-center font-bold underline'>Password Generator</h1>
+
       
+
     </>
   )
 }
